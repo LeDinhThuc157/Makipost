5d9cce0afb291ab5510b3a3d74b4e58c2665c790
nguyentea <81692943+nguyentea@users.noreply.github.com>
2023-02-02 23:29:11 +0700

    update control

--- lib/view/ControlPage.dart
+++ lib/view/ControlPage.dart
@@ -1,29 +1,70 @@
+
 import 'package:flutter/material.dart';
 import '../theme/colors.dart';
 import '../widgets/custom_appbar.dart';
+import 'package:makipos/common/form_row.dart';
 
 class  ControlPage extends StatefulWidget {
+
+  ControlPage(this.trailing, this.name, {this.isOn = false});
+
+  final Widget? trailing;
+  String name;
+  bool isOn;
   @override
-  _ControlPageState createState() => _ControlPageState();
+  State<ControlPage> createState() => _ControlPageState();
 }
+final List<ControlPage> appliances = [
+  ControlPage(Icon(Icons.battery_charging_full), 'Charge'),
+  ControlPage(Icon(Icons.battery_saver), 'Discharge'),
+  ControlPage(Icon(Icons.balance), 'Balance'),
+  ControlPage(Icon(Icons.emergency), 'Emerfency'),
+  ControlPage(Icon(Icons.heat_pump),'Heating'),
+  ControlPage(Icon(Icons.device_thermostat), 'Disable Temp.Sensor'),
+  ControlPage(Icon(Icons.heart_broken), 'GPS Heartbeat'),
+  ControlPage(Icon(Icons.desktop_windows), 'Display Always On'),
+];
 
 class _ControlPageState extends State<ControlPage> {
   @override
+
+
+
   Widget build(BuildContext context) {
     double  heightR,widthR;
     heightR = MediaQuery.of(context).size.height / 1080; //v26
     widthR = MediaQuery.of(context).size.width / 2400;
     var curR = widthR;
+
     return Scaffold(
       appBar: CustomAppbar(),
-      backgroundColor: Colors.black45,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 5*heightR,),
-          ],
-        ),
-      ), //Center
+      backgroundColor: Colors.white,
+      body: Center(
+          child: ListView.builder(
+          itemCount: appliances.length,
+          itemBuilder: (context, index) {
+            return SwitchListTile(
+
+              title: Text('${appliances[index].name}'),
+
+              value: appliances[index].isOn,
+
+              onChanged: (bool value) {
+                setState(() {
+                  appliances[index].isOn = value;
+                });
+              },
+              secondary: appliances[index].trailing,
+            );
+
+           },
+        )
+    ),
     );
   }
 }
+
+
+
+
+
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -136,7 +136,7 @@ class _HomeState extends State<Home> {
         children: <Widget>[
           StatusPage(),
           SettingsPage(),
-          ControlPage(),
+          ControlPage(Icon(Icons.cached), 'Cha'),
         ],
       );
   }
